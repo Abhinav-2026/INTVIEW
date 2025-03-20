306eda68edbdf42ca8bc60dc112140fbf53bb283
Abhinav-2026 <121487935+Abhinav-2026@users.noreply.github.com>
2025-03-20 07:01:32 +0530

    Update TeamCard.js

--- components/TeamCard.js
+++ components/TeamCard.js
@@ -4,7 +4,7 @@ import React from 'react'
 function TeamCard({ info }) {
     return (
         <div className='teamCard'>
-                <img  src={info.img} alt="" className='h-[220] w-[220]'/>
+                <img  src={info.img} alt="" className='h-[220px] w-[220px]'/>
             <h3 className='lg:text-2xl text-xl font-semibold leading-tight mt-5 text-white text-center'>
                 {info.name}
             </h3>
